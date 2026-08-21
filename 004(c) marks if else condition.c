c3d49c330fe06802a5b21f47681bef2ced4a6c1d
lakshpatel862 <lakshpatel862@gmail.com>
2026-08-21 10:27:19 +0700

    Implement if-else condition for student marks

---
+++ 004(c) marks if else condition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	//declare variablse
+	 float marks;
+	 
+	 printf("enter the marks");
+	 scanf("%f",&marks);
+	 
+	 if(marks>=30)
+	  {
+	  	printf("%f the student is pass",marks);
+	  }
+	 else
+	  {
+	  	printf("%f the student is fail",marks);
+     }
+     return 0;
+}
